e0242222091ac394c9951598e90022bcb79608c2
rserafim <rafael_reale@yahoo.com.br>
2022-02-03 20:07:48 -0300

    change 8

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 module.exports = {
   async redirects() {
     return [
-      // if the header `x-redirect-me` is present,
-      // this redirect will be applied
       {
         source: '/about',
         has: [
@@ -11,10 +9,15 @@ module.exports = {
             key: 'User-Agent',
             value: 'Mozilla/5.0 (compatible; woorankreview/2.0; +https://www.woorank.com/)',
           },
+          {
+            type: 'header',
+            key: 'User-Agent',
+            value: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36',
+          }
         ],
-        permanent: false,
         destination: '/401',
-      },
+        permanent: false
+      }
     ]
-  },
+  }
 }
