664f24194905d615ab26135bcee1892d6a18cb37
rserafim <rafael_reale@yahoo.com.br>
2022-02-05 22:28:15 -0300

    change 21

--- next.config.js
+++ next.config.js
@@ -39,9 +39,9 @@ module.exports = {
         "source": "/xyz",
         "has": [
           {
-            "type": "header",
-            "key": "x-matched-path",
-            "value": "/401"
+            "type": 'header',
+            "key": 'User-Agent',
+            "value": 'Mozilla/5.0'
           }
         ],
         "destination": "/",
