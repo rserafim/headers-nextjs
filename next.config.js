e3ee60f05c7a951d5a9fde01b53f0384b290aed3
rserafim <rafael_reale@yahoo.com.br>
2022-02-05 22:15:33 -0300

    change 19

--- next.config.js
+++ next.config.js
@@ -1,4 +1,26 @@
 module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/about',
+        headers: [
+          {
+            key: 'X-About-Custom-Header',
+            value: 'about_header_value',
+          },
+        ],
+      },
+      {
+        source: '/news/:id',
+        headers: [
+          {
+            key: 'X-News-Custom-Header',
+            value: 'news_header_value',
+          },
+        ],
+      },
+    ]
+  },
   async redirects() {
     return [
       {
@@ -6,8 +28,20 @@ module.exports = {
         "has": [
           {
             "type": "header",
-            "key": "x-powered-by",
-            "value": "Next.js"
+            "key": "x-vercel-ip-country",
+            "value": "GB"
+          }
+        ],
+        "destination": "/401",
+        "permanent": false
+      },
+      {
+        "source": "/xyz",
+        "has": [
+          {
+            "type": "header",
+            "key": "x-matched-path",
+            "value": "/401"
           }
         ],
         "destination": "/",
